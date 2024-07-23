297ec441ddfb1dec2f68985d974d76d305dc9979
JoseMiguelEM <jose.espinozam@pucp.edu.pe>
2024-07-23 13:14:00 -0400

    configuración inicial del proyecto

---
+++ backend/retoTecnicoFractal/src/main/java/com/api/ordenes/retoTecnicoFractal/RetoTecnicoFractalApplication.java
@@ -0,0 +1,13 @@
+package com.api.ordenes.retoTecnicoFractal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoTecnicoFractalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoTecnicoFractalApplication.class, args);
+	}
+
+}
---
+++ backend/retoTecnicoFractal/src/test/java/com/api/ordenes/retoTecnicoFractal/RetoTecnicoFractalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ordenes.retoTecnicoFractal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoTecnicoFractalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
